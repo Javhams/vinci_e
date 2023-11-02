a89096474ed6f94f4c756ddfc9b5db96363fe167
Javi C <secu1jv@gmail.com>
2023-11-02 07:46:29 -0400

    Filters and Navigation Nav its completed.
    For now I gonna leave this branch alone because I need to start building the backend and the resource in a new branch.

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -18,17 +18,17 @@ const Filters = () => {
   console.log({ active })
 
   return (
-    <ul className="text-white-800 body-text no-scrollbar flex
-            w-full max-w-full max-w-full gap-2 overflow-auto
-            py-12 sm:max-w-2x1">
+    <ul className="text-white-800 body-text no-scrollbar
+        flex w-full 
+        max-w-full gap-2 overflow-auto py-12 sm:max-w-2xl">
         {links.map((link)=> (
             <button
                 key={link}
                 onClick={() => handleFilter(link)}
-                className={` ${active === link ? 
-                "gradient_blue-purple" : ""}
-                    whitespace-nowrap rounded-lg
-                px-8 py-2.5 capitalize`}
+                className={`${
+                    active === link ?"gradient_blue-purple" : ""
+                  } whitespace-nowrap rounded-lg px-8 py-2.5
+                   capitalize`}
             > 
                 {link}
             </button>
